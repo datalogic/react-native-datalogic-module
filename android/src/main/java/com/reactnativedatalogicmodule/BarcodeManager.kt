3269a0fcf7dfc12882445c786bb56cedc1d99ec1
Seth Ray <seth.ray@intern.datalogic.com>
2020-09-21 09:15:54 -0700

    Working on addReadListener fix

--- android/src/main/java/com/reactnativedatalogicmodule/BarcodeManager.kt
+++ android/src/main/java/com/reactnativedatalogicmodule/BarcodeManager.kt
@@ -22,98 +22,84 @@ import android.util.Log
 
 
 
-class BarcodeManager(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class BarcodeManager(var reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    private var decoder: com.datalogic.decode.BarcodeManager? = null
-    private var listener: ReadListener? = null
+  //private var reactContext: ReactApplicationContext? = null
+  private var decoder: com.datalogic.decode.BarcodeManager? = null
+  private var listener: ReadListener? = null
 
-    override fun getName(): String {
-        return "BarcodeManager"
-    }
+  //I think Kotlins built in primary constructor is good enough?
+  //I'm not sure if I need to super the context
 
-    fun sendEvent(reactContext: ReactApplicationContext, eventName: String, params: WritableMap?) {
-      reactContext
-        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-        .emit(eventName, params)
-    }
+  override fun getName(): String {
+      return "BarcodeManager"
+  }
 
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
+  fun sendEvent(reactContext: ReactApplicationContext, eventName: String, params: WritableMap?) {
+    reactContext
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+      .emit(eventName, params)
+  }
+
+  // Example method
+  // See https://facebook.github.io/react-native/docs/native-modules-android
 //    @ReactMethod
 //    fun multiply(a: Int, b: Int, promise: Promise) {
 //      promise.resolve(a * b)
 //    }
 
-    @ReactMethod
-    fun pressTrigger(promise: Promise) {
-      try {
-        if(decoder == null) {
-          decoder = com.datalogic.decode.BarcodeManager()
-        }
-        promise.resolve(decoder?.pressTrigger())
-      } catch (e: Exception) {
-        promise.reject(e)
+  @ReactMethod
+  fun pressTrigger(promise: Promise) {
+    try {
+      if(decoder == null) {
+        decoder = com.datalogic.decode.BarcodeManager()
       }
+      promise.resolve(decoder?.pressTrigger())
+    } catch (e: Exception) {
+      promise.reject(e)
     }
+  }
 
-    @ReactMethod
-    fun releaseTrigger(promise: Promise) {
-      try {
-        if(decoder == null) {
-          decoder = com.datalogic.decode.BarcodeManager()
-        }
-        promise.resolve(decoder?.releaseTrigger())
-      } catch (e: Exception) {
-        promise.reject(e)
+  @ReactMethod
+  fun releaseTrigger(promise: Promise) {
+    try {
+      if(decoder == null) {
+        decoder = com.datalogic.decode.BarcodeManager()
       }
+      promise.resolve(decoder?.releaseTrigger())
+    } catch (e: Exception) {
+      promise.reject(e)
     }
+  }
 
-    @ReactMethod
-    fun addReadListener(successCallback: Callback, errorCallback: Callback) {
-      Log.d("TAG", "In addReadListener")
-      try {
-        if(decoder == null) {
-          decoder = com.datalogic.decode.BarcodeManager()
-        }
-        if(listener == null) {
-          // Create an anonymous class.
-          listener = ReadListener { decodeResult ->
-            try {
-              //Override so read info is put into JSON Object
-              val barcodeObject: WritableMap = WritableNativeMap()
-              barcodeObject.putString("barcodeData", decodeResult.getText())
-              barcodeObject.putString("barcodeType", decodeResult.getBarcodeID().name)
-              //Then invoke the success callback
-              successCallback.invoke(barcodeObject)
-            } catch (e: Exception) {
-              //Invoke the error callback?
-              errorCallback.invoke(e.toString())
-            }
+  @ReactMethod
+  fun addReadListener(promise: Promise) {
+    try {
+      if(decoder == null) {
+        decoder = com.datalogic.decode.BarcodeManager()
+      }
+      if(listener == null) {
+        // Create an anonymous class.
+        listener = ReadListener { decodeResult ->
+          try {
+            //Override so read info is put into JSON Object
+            val barcodeObject: WritableMap = WritableNativeMap()
+            barcodeObject.putString("barcodeData", decodeResult.getText())
+            barcodeObject.putString("barcodeType", decodeResult.getBarcodeID().name)
+            //Then invoke the success callback
+            //successCallback.invoke(barcodeObject)
+
+            sendEvent(reactContext, "successCallback", barcodeObject)
+
+          } catch (e: Exception) {
+
           }
-//          listener = object : ReadListener(){
-//            //Implement the callback method
-//            //@Override
-//            override fun onRead(decodeResult: DecodeResult) {
-//              try {
-//                //Override so read info is put into JSON Object
-//                val barcodeObject: WritableMap = WritableNativeMap()
-//                barcodeObject.putString("barcodeData", decodeResult.getText())
-//                barcodeObject.putString("barcodeType", decodeResult.getBarcodeID().name)
-//                //Then invoke the success callback
-//                successCallback.invoke(barcodeObject)
-//              } catch (e: Exception) {
-//                //Invoke the error callback?
-//                errorCallback.invoke(e.toString())
-//              }
-//            }
-//          }
         }
-        decoder!!.addReadListener(listener)
-        //successCallback.invoke("This is a test")
-        //promise.resolve(decoder!!.addReadListener(listener))
-        } catch (e: Exception) {
-          //promise.reject(e)
-          errorCallback.invoke(e.toString())
-        }
+      }
+      //decoder!!.addReadListener(listener)
+      promise.resolve(decoder!!.addReadListener(listener))
+    } catch (e: Exception) {
+      promise.reject(e)
     }
+  }
 }
